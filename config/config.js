9d246ea473b81187574d2413913aeee363f0f957
PerterB <corksurf@gmail.com>
2014-01-13 16:05:55 +0000

    Updated to use JSON API instead of XML

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	hostname: "hostname", 
+	instance: "VersionOne",
+	username: "admin",
+	password: "admin",
+	port: "80",
+	protocol: "http",
+	status: 'Prepared',
+	scope: 'Release Name',
+	room: 'TeamRoom'
+};
--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 var VersionOne = require('./lib/VersionOne').VersionOne,
-    v1,
-    hostname = "VersionOneHostName",
-    instance = "VersionOne",
-    username = "admin",
-    password = "admin",
-    port = "80",
-    protocol = "http";
-
+	config = require('./config/config'),
+    v1;
+	
 var client = require('socket.io-client');
 
 var socket = client.connect('http://localhost:3000');
 
 // join the room
-socket.emit('room', 'TeamRoom');
+socket.emit('room', config.room);
 
 
 //When a backlog request event is fired, fire a backlogResponse
 socket.on('backlogRequest', function() {
 
-	v1 = new VersionOne(hostname, instance, username, password, port, protocol);
+	v1 = new VersionOne(config.hostname, config.instance, config.username, config.password, config.port, config.protocol);
 
 	v1.query({
-		select: ['Name', 'Number', 'Status.Name', 'Scope.Name', 'Estimate'],
+		select: [],
 		from: 'Story',
 		where: {
-			'Status.Name': 'Prepared',
-			'Scope.Name': 'My Project Name'
+			'Status.Name': config.status,
+			'Scope.Name': config.scope
 		},
 		success: function(results) {
 
-			var backlogs = [];
-			if (results && results.length > 0) {
-				for (var i = 0; i < results.length; i++) {
+			var backlogs = [],
+				v1Backlogs = JSON.parse(results);
+			
+			if (v1Backlogs.Assets && v1Backlogs.Assets.length > 0) {
+				for (var i = 0; i < v1Backlogs.Assets.length; i++) {
+					var Asset = v1Backlogs.Assets[i];
+					
 					backlogs.push({
-						id: results[i].Number,
-						title: results[i].Name
+						id: Asset.Attributes.Number.value,
+						href: v1.buildBacklogUrl(Asset.id),
+						title: Asset.Attributes.Name.value,
+						description: Asset.Attributes.Description.value
 					});
 				}
 			}
-
+			
 			socket.emit('backlogResponse', backlogs);
 		},
 		failure: function(response, body) {
